57a2e4d6a6c7c19922e37b959ee37e6c77df6657
CodeWithEhtisham <shamikhan0347@gmail.com>
2022-09-09 17:43:48 +0500

    initial update

--- Customers/models.py
+++ Customers/models.py
@@ -5,7 +5,7 @@ class Customers(models.Model):
     customer_name = models.CharField(max_length=25)
     customer_rank = models.CharField(max_length=20)
     customer_id = models.CharField(max_length=10)
-    customer_file = models.FileField(upload_to='media/')
+    customer_file = models.FileField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return self.customer_name
--- Customers/views.py
+++ Customers/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+from django.http import HttpResponse
+from django.contrib import messages
+# from .forms import CustomersForm
+from .models import Customers
+
 
 ##################################### Add & details Customer Functions ##########################################
 def index(request):
     return render(request, "index.html")
 
 def customers(request):
-    return render(request, "Customers/customers.html")
+    # form_class = CustomersForm
+    # form = CustomersForm(request.POST, request.FILES or None)
+    if request.method == 'POST':
+        # if form.is_valid():
+        customer_name=request.POST.get('customer_name')
+        customer_rank=request.POST.get('customer_rank')
+        customer_id=request.POST.get('customer_id')
+        customer_file=request.POST.get['customer_file']
+        # form=CustomersForm(customer_name=customer_name, customer_rank=customer_rank, customer_id=customer_id, customer_file=customer_file)
+        # form.save()
+        messages.success(request, 'Customer added successfully')
+        return redirect('Customers:customer_details')
+        # else:
+        #     print(form.errors)
+        #     messages.error(request, 'Error adding customer')
+    else:
+        data = Customers.objects.all()
+        return render(request, "Customers/customers.html", {'data': data})
 
 def customer_details(request):
     return render(request, "Customers/customer_details.html")
