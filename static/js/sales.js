eac338591478c315bb8cc8221d713270ad6bab58
abidkhan03 <khansahb5566@gmail.com>
2022-09-17 15:55:16 +0500

    edit customer function

--- static/js/sales.js
+++ static/js/sales.js
@@ -27,31 +27,34 @@ function update_table(data){
     Object.keys(data).forEach(key => {
         elem = data[key];
         console.log(elem);
-        row = '<tr>' +
-        // '<td>'+elem['id']+'</td>' +
-        '<td>'+elem['bill_no']+'</td>' +
-        '<td>'+elem['month']+'</td>' +
-        '<td>'+elem['customer_id']['customer_rank']+'</td>' +
-        '<td>'+elem['PoS_no']+'</td>' +
-        '<td>'+elem['customer_id']['customer_name']+'</td>' +
-        // '<td>'+elem['customer_id']+'</td>' +
-        '<td>'+elem['address']+'</td>' +
-        '<td>'+elem['account_of']+'</td>' +
-        '<td>'+elem['date']+'</td>' +
-        '<td>'+elem['amount']+'</td>' +
-        '<td>'+elem['discount']+'</td>' +
-        '<td>'+elem['net_amount']+'</td>' +
-        '<td>'+elem['remarks']+'</td>' +
-        '<td>' +
-            '<div class="list">'+
-                '<a href="" class="">Edit</a>'+
-                '<button id="paid_modal" class="modal">Paid</button>'+
-                '<a href="" class="">Complemantery</a>'+
-                '<a href="" class="">Cancelled</a>'+
-            '</div>'+
-        '</td>' +
-        '</tr>'
-        all_rows += row;
+        console.log(elem['amount']>0);
+        if (elem['amount'] > 0 ){
+            row = '<tr>' +
+            // '<td>'+elem['id']+'</td>' +
+            '<td>'+elem['bill_no']+'</td>' +
+            '<td>'+elem['month']+'</td>' +
+            '<td>'+elem['customer_id']['customer_rank']+'</td>' +
+            '<td>'+elem['PoS_no']+'</td>' +
+            '<td>'+elem['customer_id']['customer_name']+'</td>' +
+            // '<td>'+elem['customer_id']+'</td>' +
+            '<td>'+elem['address']+'</td>' +
+            '<td>'+elem['account_of']+'</td>' +
+            '<td>'+elem['date']+'</td>' ;
+            '<td>'+elem['amount']+'</td>' +
+            '<td>'+elem['discount']+'</td>' +
+            '<td>'+elem['net_amount']+'</td>' +
+            '<td>'+elem['remarks']+'</td>' +
+            '<td>' +
+                '<div class="list">'+
+                    '<a href="" class="">Edit</a>'+
+                    '<button id="paid_modal" class="modal">Paid</button>'+
+                    '<a href="" class="">Complemantery</a>'+
+                    '<a href="" class="">Cancelled</a>'+
+                '</div>'+
+            '</td>' +
+            '</tr>' 
+            all_rows += row;
+    }
     });
     $('#Sales_data tbody').html(all_rows);
         
