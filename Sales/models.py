93b0efdc7a32754b8e4fbb27b5c43d055fb86957
abidkhan03 <khansahb5566@gmail.com>
2022-09-21 11:55:33 +0500

    some changes

--- Sales/models.py
+++ Sales/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from Customers.models import Customers
 
-
 class Sales(models.Model):
     bill_no = models.CharField(max_length=50, null=False, blank=False)
     PoS_no = models.CharField(max_length=50, null=False, blank=False)
@@ -15,4 +14,14 @@ class Sales(models.Model):
     remarks = models.CharField(max_length=50, null=True, blank=True)
     
     customer_id = models.ForeignKey(Customers, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Bills(models.Model):
+    rv_no = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    amount = models.PositiveIntegerField()
+    bill_remarks = models.CharField(max_length=50, null=True, blank=True)
+    reason = models.CharField(max_length=50, null=True, blank=True)
+    sales_id = models.ForeignKey(Sales, on_delete=models.CASCADE, null=True, blank=True)
+    
 
